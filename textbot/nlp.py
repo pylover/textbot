f662ed85cf2da8d07e94265e44d22632da43ebc1
pxrsw <parsaahani@aut.ac.com>
2020-04-20 02:14:40 +0430

    Start NLP part of project #10

---
+++ textbot/nlp.py
@@ -0,0 +1,13 @@
+import spacy
+
+
+def speechtagging(input):
+    result = []
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(input)
+
+    for token in doc:
+        if token.pos_ == "VERB" or token.pos_ == "NOUN":
+            result.append([token.text, token.pos_])
+
+    return result
