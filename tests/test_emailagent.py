c3189082946798d06726fbd29d7842085f4e086c
pxrsw <parsaahani@aut.ac.com>
2020-04-30 16:25:08 +0430

    Edit one line of test_emailagent.py file #18

--- tests/test_emailagent.py
+++ tests/test_emailagent.py
@@ -2,7 +2,8 @@ from textbot import EmailAgent
 
 
 def test_emailagent(smtpserver):
-    host, port = smtpserver.addr
+    host=smtpserver.addr[0]
+    port=smtpserver.addr[1]
     username = 'foo@example.com'
     password = 'foo'
     agent = EmailAgent(host, port, username, password)
